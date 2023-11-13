91a9ab439c0dd1fd74d8d7c17718d9e1df1c1e34
Deras879 <derycard59@gmail.com>
2023-11-12 20:58:05 -0500

    Finalizado HW sequelize Parte 1

--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
@@ -1,5 +1,30 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
-  sequelize.define('Ability', {});
+  sequelize.define('Ability', {
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description:{
+      type: DataTypes.TEXT,
+    },
+    mana_cost: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isFloat(value){
+           if(value < 10.0 || value > 250.0){
+              throw new Error('El valor debe estar entre 10.0 y 250.0')
+           }
+        }
+     }
+    }
+  },{
+    indexes: [{
+      unique: true,
+      fields: ['name', 'mana_cost'],
+      name: "compositeIndex"
+    }]
+  });
 };
--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
@@ -1,5 +1,47 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('Character', {});
+   sequelize.define('Character', {
+      code: {
+        type: DataTypes.STRING(5),
+        primaryKey: true,
+        validate: {
+         notIn(value) {
+         if (value === "HENRY" || value === "henry") {
+           throw new Error('No puede ser Henry');
+         }
+       }
+      }
+      },
+      name:{
+         type: DataTypes.STRING,
+         unique:true,
+         allowNull: false,
+         validate: {
+            notIn(value) {
+            if (value === "Henry" || value === "SoyHenry" || value === "Soy Henry") {
+              throw new Error('No puede ser [Henry, SoyHenry, Soy Henry]');
+            }
+          }
+         }
+      },
+      age:{
+         type: DataTypes.INTEGER,
+      },
+      race: {
+         type: DataTypes.ENUM ('Human', 'Elf', 'Machine', 'Demon', 'Animal', 'Other'),
+         defaultValue: "Other"
+      },
+      hp:{
+         type: DataTypes.FLOAT,
+         allowNull: false,
+      },
+      mana: {
+         type: DataTypes.FLOAT,
+         allowNull: false,
+      
+      }
+   },{
+      timestamps:false
+   });
 };
